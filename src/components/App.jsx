97e5bf25a2f5111bee17813c5e57d9d8b5fda613
Atunde Toheeb Ayomide <atundetoheeb1@gmail.com>
2025-10-15 20:30:07 +0100

    commit for readme and title change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,7 @@ export default function App(){
   return(
     <main>
       <header>
-        <h1>Assembly: Endgame</h1>
+        <h1>Code Extinct</h1>
         <p>Guess the word within 8 attempts to keep the 
           programming world safe from Assembly!</p>
       </header>
