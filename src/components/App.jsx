36b0d42e9ab87d203e5fe94f55bf0140d0414e16
Atunde Toheeb Ayomide <atundetoheeb1@gmail.com>
2025-10-15 20:16:13 +0100

    initial commit for assembly: endgame

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import { languages } from "./languages"
+
+export default function App(){
+
+
+  const languageElements = languages.map((lang) => (
+  <div 
+  className="chip" 
+  style={{backgroundColor: lang.backgroundColor, color: lang.color}}
+  key={lang.name}
+  >
+    {lang.name}
+  </div>
+  ))
+
+  return(
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word within 8 attempts to keep the 
+          programming world safe from Assembly!</p>
+      </header>
+      <section className="game-status"> 
+          <h2>You Win!</h2>
+          <p>restart the game</p>
+      </section>
+
+
+      <section className="language-chips">
+        {languageElements}
+      </section>
+    </main>
+  )
+}
